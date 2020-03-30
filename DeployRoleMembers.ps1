7a09dc68f8f8fb7462f2359d2371d0e7789de4ee
Alex Yates <alex.yates@dlmconsultants.com>
2020-03-30 10:22:34 +0100

    Deploy the role members!

--- DeployRoleMembers.ps1
+++ DeployRoleMembers.ps1
@@ -6,19 +6,104 @@ param(
     [switch]$DeleteAdditional = $false
 )
 
-
 import-module dbatools
 $ErrorActionPreference = "stop"
-
 Test-DbaConnection $SQLInstance | out-null
 
-$RoleMembersFile = Join-Path -Path $SourceDir -ChildPath "rolemembers_$Environment.json"
+# Read source role members from source directory
 
-if (-not(Test-Path -path $RoleMembersFile)){
-    Write-Error "No source file found at $RoleMembersFile"
+$roleMembersFile = Join-Path -Path $SourceDir -ChildPath "rolemembers_$Environment.json"
+if (-not(Test-Path -path $roleMembersFile)){
+    Write-Error "No source file found at $roleMembersFile"
+}
+Write-Output "Reading source role members"
+$sourceRoleMembers = Get-Content $roleMembersFile | ConvertFrom-Json
+
+# Get existing role members from target DB and refactor to match source data structure
+
+Write-Output "Reading database role members"
+$rawDbRoleMembers = Get-DbaDbRoleMember -SqlInstance $SQLInstance -Database $Database
+
+$simpleDbRoleMembers = @()
+For ($i=0; $i -lt $rawDbRoleMembers.Length; $i++){    
+    # Pulling all the role memberships from the DB
+    if ($simpleDbRoleMembers.Role -notcontains $rawDbRoleMembers[$i].Role){
+        $tempRole = New-Object PSObject -Property @{
+            Role = $rawDbRoleMembers[$i].Role
+            Members = @()
+        }
+        # Adding all the members to the temp role
+        For ($j=0; $j -lt $rawDbRoleMembers.Length; $j++){
+            if($rawDbRoleMembers[$j].Role -like $rawDbRoleMembers[$i].Role){
+                if ($rawDbRoleMembers[$j].UserName -notin $tempRole.Members){
+                    $tempRole.Members += $rawDbRoleMembers[$j].UserName
+                }
+            }
+        }
+        # Adding the temp role to new members
+        $simpleDbRoleMembers = $simpleDbRoleMembers + $tempRole
+    }
 }
 
-Write-Output "Reading source users"
-$SourceRoleMembers = Get-Content $RoleMembersFile | ConvertFrom-Json
+# First, add any role members that dont exist on target
 
-Write-Warning "To do: Write the code to deplopy role members"
+for ($i=0; $i -lt $sourceRoleMembers.Length; $i++){
+    $IdMatch = [array]::IndexOf($simpleDbRoleMembers.Role,$sourceRoleMembers[$i].Role)
+    if ($IdMatch -eq -1){
+        # The role does not exist on the target database!
+        Write-Warning $sourceRoleMembers[$i].Role " does not exist on $SQLInstance.$Database"
+        Write-Output "    Unable to add the following members to " $sourceRoleMembers[$i].Role ":"
+        foreach ($member in $sourceRoleMembers[$i].Members){
+            Write-Output "        - $member"
+        }
+    }
+    else {
+        # Add any members that are missing on the target database.
+        foreach ($member in $sourceRoleMembers[$i].Members){
+            if($member -notin $simpleDbRoleMembers[$IdMatch].Members){
+                Write-Host "Adding user $member to role " $sourceRoleMembers[$i].Role " on $SQLInstance.$Database."
+                Add-DbaDbRoleMember -SqlInstance $SQLInstance -Database $Database -Role $sourceRoleMembers[$i].Role -User $member
+            }
+        }
+    }
+}
+
+# Then, check whether any need removing. If $DeleteAdditional, remove - otherwise just warn.
+
+for ($i=0; $i -lt $simpleDbRoleMembers.Length; $i++){
+    $IdMatch = [array]::IndexOf($sourceRoleMembers.Role,$simpleDbRoleMembers[$i].Role)
+    if ($IdMatch -eq -1){
+        # The role does not exist in the source!
+        Write-Warning $simpleDbRoleMembers[$i].Role " does not have any users in source, but it does have users on $SQLInstance.$Database"
+        if ($DeleteAdditional){
+            Write-Output "    Removing the following users from " $sourceRoleMembers[$i].Role ":"
+            foreach ($member in $simpleDbRoleMembers[$i].Members){
+                Write-Output "        - $member"
+                Remove-DbaDbRoleMember -SqlInstance $SQLInstance -Database $Database -Role $sourceRoleMembers[$i].Role -User $member
+            }
+        }
+        else {
+            Write-Output "    The following users need to be removed from " $sourceRoleMembers[$i].Role ":"
+            foreach ($member in $simpleDbRoleMembers[$i].Members){
+                Write-Output "        - $member"
+            }
+        }
+    }
+    else {
+        # The role exists in the source
+        foreach ($member in $simpleDbRoleMembers[$i].Members){
+            if($member -notin $sourceRoleMembers[$IdMatch].Members){
+                # The member exists on the target database but not in the source
+                Write-Warning $simpleDbRoleMembers[$i].Role " contains $member on $SQLInstance.$Database but this role member does not exist in source."
+                if ($DeleteAdditional){
+                    Write-Output "    Removing $member from role " $simpleDbRoleMembers[$i].Role "."
+                    Remove-DbaDbRoleMember -SqlInstance $SQLInstance -Database $Database -Role $simpleDbRoleMembers[$i].Role -User $member
+                    }
+                }
+                else {
+                    Write-Output "    $member needs to be removed from " $sourceRoleMembers[$i].Role "."
+                }
+            }
+        }
+    }
+}
